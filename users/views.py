7434844c31bf07a9c477c95cd8e83c14a28e885d
jylu lyapunova <jylilapa@yandex.ru>
2025-05-09 18:32:05 +0300

    add workflow

--- users/views.py
+++ users/views.py
@@ -1,7 +1,5 @@
-from rest_framework import filters
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ModelViewSet
 
 from users.models import Payment, User
 from users.serializers import PaymentSerializer, UserSerializer
