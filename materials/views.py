b0b4e9b01cd189392aa8e04055be856d5ab2ccb0
jylu lyapunova <jylilapa@yandex.ru>
2025-05-11 14:18:19 +0300

    fixed

--- materials/views.py
+++ materials/views.py
@@ -1,16 +1,19 @@
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveAPIView,
-                                     UpdateAPIView, get_object_or_404)
+                                     UpdateAPIView)
+# from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
-from materials.models import Course, Lesson, Subscription
+from materials.models import Course, Lesson
+# from materials.models import Subscription
 from materials.paginators import CustomPagination
 from materials.serializers import (CourseDetailSerializer, CourseSerializer,
-                                   LessonSerializer, SubscriptionSerializer)
-from materials.tasks import send_subscription
+                                   LessonSerializer)
+# from materials.serializers import SubscriptionSerializer
+# from materials.tasks import send_subscription
 from users.permissions import IsOwner, Moder
 
 
