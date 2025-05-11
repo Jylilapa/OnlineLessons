fc23ae06599e6bbd2692a8af710b844047a999d4
jylu lyapunova <jylilapa@yandex.ru>
2025-05-11 15:34:49 +0300

    fixed

--- materials/tests.py
+++ materials/tests.py
@@ -3,7 +3,6 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from materials.models import Course, Lesson
-# from materials.models import Subscription
 from users.models import User
 
 
